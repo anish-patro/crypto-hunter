b85b0482eccc179261da864b37e1f3d11174566b
anish <anishkumarpatro03@gmail.com>
2023-05-02 12:17:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import CoinPage from './pages/CoinPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+        <Route path='/' component={HomePage} exact/> 
+        <Route path='/Coins/:id' component={CoinPage}/> 
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>absbsb</div>
+  )
+}
+
+export default HomePage;
